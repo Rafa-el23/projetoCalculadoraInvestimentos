62d9d8c460d3a4476bac99e4f1e83abdc4f9b20a
Rafael Barbosa <rafaeldecristo23@gmail.com>
2026-04-03 20:17:46 -0300

    processos relacionados aos gráficos foram comentados

--- main.js
+++ main.js
@@ -173,5 +173,5 @@ for (const formElement of form) {
   }
 }
 
-form.addEventListener('submit', renderProgression);
+// form.addEventListener('submit', renderProgression);
 clearFormButton.addEventListener('click', clearForm);
