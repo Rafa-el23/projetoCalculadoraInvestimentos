b445051afbe87ee225b6bca516d3288dc6f35e48
Rafael Barbosa <rafaeldecristo23@gmail.com>
2026-04-06 20:19:21 -0300

    Implemetada a funcionalidade do carrossel

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import { generateReturnsArray } from './src/investmentGoals';
 import { Chart } from 'chart.js/auto';
+import { createTable } from './src/table.js';
 
 const finalMoneyChart = document.getElementById('final-money-distribution');
 const progressionChart = document.getElementById('progression');
@@ -8,7 +9,35 @@ const clearFormButton = document.getElementById('clear-form');
 let doughnutChartReference = {};
 let progressionChartReference = {};
 
-function formatCurrency(value) {
+const columnsArray = [
+  { columnLabel: 'Mês', accessor: 'month' },
+  {
+    columnLabel: 'Total Investido',
+    accessor: 'investedAmount',
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
+  },
+  {
+    columnLabel: 'Rendimento Mensal',
+    accessor: 'interestReturns',
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
+  },
+  {
+    columnLabel: 'Rendimento Total',
+    accessor: 'totalInterestReturns',
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
+  },
+  {
+    columnLabel: 'Quantia Total',
+    accessor: 'totalAmount',
+    format: (numberInfo) => formatCurrencyToTable(numberInfo),
+  },
+];
+
+function formatCurrencyToTable(value) {
+  return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+}
+
+function formatCurrencyToGraph(value) {
   return value.toFixed(2);
 }
 
@@ -54,11 +83,11 @@ function renderProgression(evt) {
       datasets: [
         {
           data: [
-            formatCurrency(finalInvestimentObject.investedAmount),
-            formatCurrency(
+            formatCurrencyToGraph(finalInvestimentObject.investedAmount),
+            formatCurrencyToGraph(
               finalInvestimentObject.totalInterestReturns * (1 - taxRate / 100),
             ),
-            formatCurrency(
+            formatCurrencyToGraph(
               finalInvestimentObject.totalInterestReturns * (taxRate / 100),
             ),
           ],
@@ -81,14 +110,14 @@ function renderProgression(evt) {
         {
           label: 'Total Investido',
           data: returnsArray.map((investmentObject) =>
-            formatCurrency(investmentObject.investedAmount),
+            formatCurrencyToGraph(investmentObject.investedAmount),
           ),
           backgroundColor: 'rgb(255, 99, 132)',
         },
         {
           label: 'Retorno do Investimento',
           data: returnsArray.map((investmentObject) =>
-            formatCurrency(investmentObject.investedAmount),
+            formatCurrencyToGraph(investmentObject.investedAmount),
           ),
           backgroundColor: 'rgb(54, 162, 235)',
         },
@@ -106,6 +135,8 @@ function renderProgression(evt) {
       },
     },
   });
+
+  createTable(columnsArray, returnsArray, 'results-table');
 }
 
 function isObjectEmpty(obj) {
@@ -173,5 +204,17 @@ for (const formElement of form) {
   }
 }
 
-// form.addEventListener('submit', renderProgression);
+const mainEl = document.querySelector('main');
+const carouselEl = document.getElementById('carousel');
+const nextButton = document.getElementById('slide-arrow-next');
+const previousButton = document.getElementById('slide-arrow-previous');
+
+nextButton.addEventListener('click', () => {
+  carouselEl.scrollLeft += mainEl.clientWidth;
+});
+previousButton.addEventListener('click', () => {
+  carouselEl.scrollLeft -= mainEl.clientWidth;
+});
+
+form.addEventListener('submit', renderProgression);
 clearFormButton.addEventListener('click', clearForm);
